67813cefe9e521b0b380a5ad57fd4600842a2c96
lmflaffitte <lmflaffitte@gmail.com>
2020-12-26 13:23:12 -0800

    Removed NMEA parsing

--- automoc.py
+++ automoc.py
@@ -82,6 +82,7 @@ def alarm(value):
 		blynk.virtual_write(25, vehicle_lat)
 		blynk.virtual_write(26, vehicle_lon)
 		blynk.virtual_write(35, vehicle_alt)
+
 	else:
 		lib8relay.set(1,8,int(value[0]))
 		blynk.virtual_write(14, "ENABLED")
@@ -98,43 +99,6 @@ def gather_gps_data():
 
 	gps_data = [lat, lon, alt, speed, climb, fixtype]
 	return gps_data
-
-def gather_gga_data():
-        ### POINTS VARIABLE TO PROPER SERIAL PORT ###
-        ser = serial.Serial("/dev/ttyACM3",115200)
-        ### WHILE LOOP TO KEEP READING DATA-STREAM ###
-        while True:
-                data = ser.readline()
-                if str(data).startswith("$GNGGA"):
-
-                        ### CONVERT DATA INTO LEGIBLE OUTPUTS###
-			msg = pynmea2.parse(data)
-                       #lat = (round(msg.latitude, 5))
-                        lat = ('%02d{}%02d\'%07.4f'.format(" ") % (msg.latitude, msg.latitude_minutes, msg.latitude_seconds))
-			lat_dir = (msg.lat_dir)
-			lat_full = lat + " " + lat_dir
-                       #lon = (round(msg.longitude, 5))
-                        lon = ('%02d{}%02d\'%07.4f'.format(" ") % (msg.longitude, msg.longitude_minutes, msg.longitude_seconds))
-			lon_dir = (msg.lon_dir)
-			lon_full = lon + " " + lon_dir
-                        alt = (msg.altitude)
-                        alt_unit = (msg.altitude_units)
-                        sats = (msg.num_sats)
-                        #print(msg)
-			return lat_full, lon_full, alt, alt_unit, sats
-
-def gather_rmc_data():
-
-	### POINTS VARIABLE TO PROPER SERIAL PORT ###
-	ser = serial.Serial("/dev/ttyACM3",115200)
-	while True:
-		data = ser.readline()
-		if str(data).startswith("$GNRMC"):
-			### CONVERT DATA INTO LEGIBLE OUTPUTS ###
-			msg = pynmea2.parse(data)
-			sog = (msg.spd_over_grnd)
-			#print(msg)
-			return sog
 
 ### SEND GPS DATA TO BLYNK ###
 @timer.register(vpin_num = 100, interval = 5, run_once = False)
